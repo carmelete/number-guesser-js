d4d4504989a842b4bc6e8d6906856b359153bdd7
carmelete96 <carmenortegarico@gmail.com>
2022-06-10 12:08:25 +0200

    add function generateTarget()

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
+function generateTarget() {
+    const max = 9;
+    const min = 0;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
