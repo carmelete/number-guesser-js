69808d2422de4259ab9583a3746978f2b48fde33
carmelete96 <carmenortegarico@gmail.com>
2022-06-13 09:28:06 +0200

    refactor: generateTarget()

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 function generateTarget() {
-    const max = 9;
-    const min = 0;
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * 10);
 }
 
 function compareGuesses(currentHumanGuess, computerGuess, target) {
